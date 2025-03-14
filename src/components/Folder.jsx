e79fa0f25f7bd222b42ea688b7e6b4d2e2492421
saadArfin <saad.arfin.ug21@nsut.ac.in>
2025-03-15 03:04:38 +0530

    add input functionality for creating new folders and files in the file explorer

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -3,14 +3,42 @@ import { useState } from "react";
 function Folder({ explorer }) {
   console.log(explorer);
   const [expand, setExpand] = useState(false);
+  const [showInput, setShowInput] = useState({
+    visible: false,
+    isFolder: null,
+  });
+
+  const handleNewFolder = (e, isFolder) => {
+    e.stopPropagation();
+    setExpand(true);
+    setShowInput({
+      visible: true,
+      isFolder,
+    });
+  };
 
   if (explorer.isFolder) {
     return (
       <div style={{ marginTop: 5 }}>
         <div className="folder" onClick={() => setExpand(!expand)}>
           <span>📁{explorer.name}</span>
+          <div>
+            <button onClick={(e) => handleNewFolder(e, true)}>Folder +</button>
+            <button onClick={(e) => handleNewFolder(e, false)}>File +</button>
+          </div>
         </div>
         <div style={{ display: expand ? "block" : "none", paddingLeft: 25 }}>
+          {showInput.visible && (
+            <div className="inputContainer">
+              <span>{showInput.isFolder ? "📁" : "📄"}</span>
+              <input
+                className="inputContainer__input"
+                type="text"
+                onBlur={() => setShowInput({ ...showInput, visible: false })}
+                autoFocus
+              />
+            </div>
+          )}
           {explorer.items.map((exp) => {
             return <Folder explorer={exp} key={exp.id} />;
           })}
